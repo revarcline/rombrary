29cc73db67cf6d54f88368312b64b7e70c59ffb5
Alex Cline <rev.a.r.cline@gmail.com>
2021-01-29 17:58:20 -0600

    got the by attribute view going

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,12 @@
 class GamesController < ApplicationController
   # GET: /games
   get '/games' do
-    erb :"/games/index.html"
+    erb :"/games/index"
   end
 
   # GET: /games/new
   get '/games/new' do
-    erb :"/games/new.html"
+    erb :"/games/new"
   end
 
   # POST: /games
@@ -14,9 +14,33 @@ class GamesController < ApplicationController
     redirect '/games'
   end
 
+  # GET: /games/genre/fighting
+  get '/games/by/:attr/:slug' do
+    @slug = params[:slug]
+    @attr = params[:attr]
+    @obj = nil
+    @gamelist = nil
+    case @attr
+    when 'genre'
+      @obj = Genre.find_by_slug(@slug)
+      @gamelist = Game.joins(:game_genres).where(game_genres: { genre_id: @obj.id })
+    when 'region'
+      @obj = Region.find_by_slug(@slug)
+      @gamelist = Game.joins(:game_regions).where(game_regions: { region: @obj.id })
+    when 'console'
+      @obj = Console.find_by_slug(@slug)
+      @gamelist = Game.where(console_id: @obj.id)
+    when 'publisher'
+      @obj = Publisher.find_by_slug(@slug)
+      @gamelist = Game.where(publisher_id: @obj.id)
+    end
+    erb :'/games/show_attr'
+  end
+
   # GET: /games/5
   get '/games/:id' do
-    erb :"/games/show.html"
+    @game = Game.find(params[:id])
+    erb :"/games/show"
   end
 
   # GET: /games/5/edit
