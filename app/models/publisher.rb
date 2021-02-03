f05c6c87d34459c505f4dd5ab2c114413e03f5f3
Alex Cline <rev.a.r.cline@gmail.com>
2021-02-03 14:42:35 -0600

    wait what just happened

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -2,7 +2,7 @@ class Publisher < ActiveRecord::Base
   include Slugifiable
 
   has_many :game_publishers
-  has_many :games, through: :publishers
+  has_many :games, through: :game_publishers
 
   validates :name, presence: true,
                    uniqueness: true
